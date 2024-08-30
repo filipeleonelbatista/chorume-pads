4053a0f38cf996e7f8ce624ac6c0bcdfc0e20fa5
Filipe Batista <filipe.x@live.com>
2024-08-29 21:50:23 -0300

    v1

---
+++ README.md
@@ -0,0 +1,30 @@
+<img width="100%" src=".github/1.png">
+
+# Indice
+
+- [Sobre](#-sobre)
+
+## 🔖&nbsp; Sobre
+
+Chorume pads, são os pads de audios usados nas lives do @ManoDeyvin do canal Mano Deyvin
+
+[Link do Site](https://filipeleonelbatista.github.io/chorume-pads/)
+
+---
+
+<h3 align="center" >Vamos nos conectar 😉</h3>
+<p align="center">
+  <a href="https://www.linkedin.com/in/filipeleonelbatista/">
+    <img alt="LinkedIn" width="22px" src="https://github.com/filipeleonelbatista/filipeleonelbatista/blob/master/assets/052-linkedin.svg" />
+  </a>&ensp;
+  <a href="mailto:filipe.x2016@gmail.com">
+    <img alt="Email" width="22px" src="https://github.com/filipeleonelbatista/filipeleonelbatista/blob/master/assets/gmail.svg" />
+  </a>&ensp;
+  <a href="https://instagram.com/filipeleonelbatista">
+    <img alt="Instagram" width="22px" src="https://github.com/filipeleonelbatista/filipeleonelbatista/blob/master/assets/044-instagram.svg" />
+  </a>
+</p>
+<br />
+<p align="center">
+    Desenvolvido 💜 por Filipe Batista 
+</p>
---
+++ main.js
@@ -0,0 +1,50 @@
+const audios = [
+  { pauseWhenAction: false, loop: false, volume: 0.3, type: 'red', name: "Ai", url: './audios/1.mp3' },
+  { pauseWhenAction: false, loop: false, volume: 0.9, type: 'red', name: "Errou!", url: './audios/2.mp3' },
+  { pauseWhenAction: false, loop: false, volume: 0.3, type: 'red', name: "Ele gosta", url: './audios/3.mp3' },
+  { pauseWhenAction: false, loop: false, volume: 0.1, type: 'blue', name: 'Lá ele', url: './audios/4.mp3' },
+  { pauseWhenAction: false, loop: false, volume: 0.9, type: 'blue', name: "Suspense", url: './audios/5.mp3' },
+  { pauseWhenAction: false, loop: false, volume: 0.1, type: 'blue', name: "Palmas", url: './audios/6.mp3' },
+  { pauseWhenAction: true, loop: false, volume: 0.3, type: 'yellow', name: 'Para de mandar audio', url: './audios/7.mp3' },
+  { pauseWhenAction: true, loop: false, volume: 0.1, type: 'yellow', name: "Faz o Pix", url: './audios/8.mp3' },
+  { pauseWhenAction: true, loop: false, volume: 0.3, type: 'yellow', name: "Momento Camarada", url: './audios/12.mp3' },
+  { pauseWhenAction: true, loop: true, volume: 0.3, type: 'box', name: "Dupeta Ei", url: './audios/9.mp3' },
+  { pauseWhenAction: true, loop: true, volume: 0.3, type: 'box', name: "Chorume News", url: './audios/10.mp3' },
+  { pauseWhenAction: true, loop: true, volume: 0.3, type: 'box', name: "Leitura duvidosa", url: './audios/11.mp3' },
+];
+
+const padlist = document.getElementById('padlist');
+const playingAudios = {};
+
+audios.forEach(audio => {
+  const button = document.createElement('button');
+  button.className = audio.type;
+  button.innerText = audio.name;
+  button.onclick = () => handleAudioAction(audio);
+  padlist.appendChild(button);
+});
+
+function handleAudioAction(audio) {
+  const { url, volume, loop, pauseWhenAction } = audio;
+
+  if (pauseWhenAction && playingAudios[url]) {
+    playingAudios[url].pause();
+    playingAudios[url].currentTime = 0;
+    delete playingAudios[url]; 
+  } else {
+    playAudio(url, volume, loop);
+  }
+}
+
+function playAudio(url, volume, loop) {
+  const audio = new Audio(url);
+  audio.volume = volume;
+  audio.loop = loop;
+  audio.play();
+
+  playingAudios[url] = audio;
+
+  audio.addEventListener('ended', () => {
+    delete playingAudios[url];
+  });
+}
